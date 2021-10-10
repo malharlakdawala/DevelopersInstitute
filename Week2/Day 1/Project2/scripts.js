15b438e6cd93df653b56b5c69e4ad37a500347ab
malhar <malharlakdawala@gmail.com>
2021-10-10 16:30:07 +0530

    js

--- Week2/Day 1/Project2/scripts.js
+++ Week2/Day 1/Project2/scripts.js
@@ -1,3 +1,4 @@
+console.log("Exc 1:")
 let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
 fruits.splice(0, 1);
 console.log("1. After deleting banana the array is: " + fruits);
@@ -8,3 +9,7 @@ fruits.shift();
 console.log("4. After removing apple, the array is: " + fruits);
 fruits.reverse();
 console.log("5. After reversing, the array is: " + fruits);
+//Exc 2
+console.log("Exc 2:")
+let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+console.log("Value of nested array is: " + moreFruits[1][1][0]);
