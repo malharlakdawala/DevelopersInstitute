197f5626992e114a2968b59160a0f1b6dfde89a0
malhar <malharlakdawala@gmail.com>
2021-10-10 16:23:46 +0530

    js

---
+++ Week2/Day 1/Project2/scripts.js
@@ -0,0 +1,10 @@
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+fruits.splice(0, 1);
+console.log("1. After deleting banana the array is: " + fruits);
+console.log("2. After sorting, the array is: " + fruits.sort());
+fruits.push("kiwi");
+console.log("3. After adding kiwi, the array is: " + fruits);
+fruits.shift();
+console.log("4. After removing apple, the array is: " + fruits);
+fruits.reverse();
+console.log("5. After reversing, the array is: " + fruits);
