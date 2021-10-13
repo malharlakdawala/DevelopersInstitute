8398118be70e543e6c54109a2ef681aba8e36d91
malhar <malharlakdawala@gmail.com>
2021-10-13 23:26:21 +0530

    exc1

--- Week2/Day4/Exc1 xp/scripts.js
+++ Week2/Day4/Exc1 xp/scripts.js
@@ -1,55 +1,70 @@
-//Exc1
-// function infoAboutMe() {
-//   console.log(`DUmmy sentence`);
-// }
+// //Exc1
+// // function infoAboutMe() {
+// //   console.log(`DUmmy sentence`);
+// // }
 
-// infoAboutMe();
+// // infoAboutMe();
 
-//Exc2
+// //Exc2
 
-// function infoAboutPerson(personName, personAge, personFavoriteColor) {
-//   console.log(
-//     `your name is ${personName} age is ${personAge} and fav colour is ${personFavoriteColor}`
-//   );
-// }
-// infoAboutPerson("David", 45, "blue")
-// infoAboutPerson("Josh", 12, "yellow")
+// // function infoAboutPerson(personName, personAge, personFavoriteColor) {
+// //   console.log(
+// //     `your name is ${personName} age is ${personAge} and fav colour is ${personFavoriteColor}`
+// //   );
+// // }
+// // infoAboutPerson("David", 45, "blue")
+// // infoAboutPerson("Josh", 12, "yellow")
 
-//Exc3
+// //Exc3
 
-// function infoAboutPerson(
-//   personName,
-//   personAge,
-//   personFavoriteColor,
-//   personHobbies
-// ) {
-//   console.log(
-//     `your name is ${personName} age is ${personAge} and fav colour is ${personFavoriteColor}. Hobbies are `
-//   );
-//   for (let i in personHobbies) {
-//     console.log(personHobbies[i]);
+// // function infoAboutPerson(
+// //   personName,
+// //   personAge,
+// //   personFavoriteColor,
+// //   personHobbies
+// // ) {
+// //   console.log(
+// //     `your name is ${personName} age is ${personAge} and fav colour is ${personFavoriteColor}. Hobbies are `
+// //   );
+// //   for (let i in personHobbies) {
+// //     console.log(personHobbies[i]);
+// //   }
+// // }
+
+// // infoAboutPerson("David", 45, "blue", ["tennis", "painting"]);
+// // infoAboutPerson("Josh", 12, "yellow", [
+// //   "videoGame",
+// //   "hanging out with friends",
+// //   "playing cards",
+// // ]);
+
+// //Exercise 2 : Keyless Car
+
+// // function checkDriverAge(age) {
+// //   if (age < 18) {
+// //     console.log("Sorry, you are too young to drive this car. Powering off");
+// //   } else if (age > 18) {
+// //     console.log("You are old enough to drive, Powering On. Enjoy the ride!");
+// //   } else {
+// //     console.log(
+// //       "Congratulations on your first year of driving. Enjoy the ride!"
+// //     );
+// //   }
+// // }
+
+// // checkDriverAge(18);
+
+// //Exercise 3: Odd Or Even
+
+// function checkNumber() {
+//   for (i = 1; i < 101; i++) {
+//     if (i % 2 == 0) {
+//       console.log(`${i} is an even number`);
+//     } else {
+//       console.log(`${i} is an odd number`);
+//     }
 //   }
 // }
+// checkNumber();
 
-// infoAboutPerson("David", 45, "blue", ["tennis", "painting"]);
-// infoAboutPerson("Josh", 12, "yellow", [
-//   "videoGame",
-//   "hanging out with friends",
-//   "playing cards",
-// ]);
-
-//Exercise 2 : Keyless Car
-
-function checkDriverAge(age) {
-  if (age < 18) {
-    console.log("Sorry, you are too young to drive this car. Powering off");
-  } else if (age > 18) {
-    console.log("You are old enough to drive, Powering On. Enjoy the ride!");
-  } else {
-    console.log(
-      "Congratulations on your first year of driving. Enjoy the ride!"
-    );
-  }
-}
-
-checkDriverAge(18);
+//Exercise 4: Find The Numbers Divisible By 23
