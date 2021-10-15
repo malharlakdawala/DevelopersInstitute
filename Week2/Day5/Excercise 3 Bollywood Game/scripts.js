679b54d4f23f1f33cd0372ff00a873a8909da850
malhar <malharlakdawala@gmail.com>
2021-10-16 00:15:04 +0530

    bollywood game

---
+++ Week2/Day5/Excercise 3 Bollywood Game/scripts.js
@@ -0,0 +1,11 @@
+let str = prompt(
+  "Player s1: Enter Name of the movie you want other person to guess"
+);
+
+console.log("*".repeat(str.length));
+
+/*
+make function 1--> validation of input movie -- input string, length minimum 8
+
+make function 2 -->
+*/
