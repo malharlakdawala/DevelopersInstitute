6b838475a5ec4dc3a2c56b5c9e943aaad47fa1e6
malhar <malharlakdawala@gmail.com>
2021-10-17 16:31:34 +0530

    DOM

---
+++ Week3/Day1/Exc1/scripts.js
@@ -0,0 +1,8 @@
+let divelement = document.body.firstElementChild;
+let divelement2 = document.getElementById("container");
+
+let ulelement1 = document.body.children[1];
+let ulelement2 = document.querySelectorAll(".list li");
+
+console.log(divelement, divelement2);
+console.log(ulelement1, ulelement2);
