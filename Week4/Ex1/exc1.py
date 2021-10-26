399a359a0522fc830821957ecda78313ae93d9c7
malhar <malharlakdawala@gmail.com>
2021-10-26 20:11:18 +0530

    py proj

---
+++ Week4/Ex1/exc1.py
@@ -0,0 +1,49 @@
+# 1
+# print("Hello world \n"*5)
+
+# 2
+# print(99**3*8)
+
+# 3
+# print(5<3)
+# print(3==3)
+# print(3=='3')
+# print("3">3)
+# print("Hello"=="hello")
+
+# 4
+# computer_brand = "dell"
+# print(f"i have a {computer_brand} computer")
+
+# # 5
+# name = "malhar"
+# age = 32
+# shoe_size = 10.5
+# info = f"hello, i am {name} aged {age} and my shoe size is {shoe_size}"
+# print(info)
+
+#6
+# a=6
+# b=7
+# if a<b:
+#     print(f"{a} is smaller")
+# else:
+#     print(f"{b} is smaller")
+
+#7
+# a = int(input("no. daalo bhai "))
+# if(a%2==0):
+#     print("number is even")
+# else:
+#     print("number is odd")
+
+#8
+# a = input("iss baar naam daalna he ")
+# print(f"didnt understand what to do {a}")
+
+#9
+a = int(input("height bolo bhai "))
+if a<145:
+    print("Tingu he re tu..")
+else:
+    print("Jaa, tu bhi kya yaad rakhega ")
--- Week4/Ex1/new.py
+++ Week4/Ex1/new.py
@@ -1,3 +1,36 @@
-name = "toby"
-age = 4
-print(name*age)
+# name = "toby"
+# age = 4
+
+# print(
+#     f"helo {name} asdf {age} asdf")
+
+
+# a = 5
+# b = 6
+# if a < b:
+#     print(f"{b} is bigger")
+# else:
+#     print(f"{a} is bigger")
+
+# #print("i love python\n"*5+"hellow rol \n"*5)
+
+# a = int(input("enter month "))
+# if 0 < a <= 3:
+#     print("its spring")
+# elif 3 < a <= 6:
+#     print("its summer")
+# elif 6 < a <= 9:
+#     print("its summer")
+# else:
+#     print("its winter")
+
+#mytext = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim"
+# print(len(mytext))
+
+a = input("input string kar bhai ")
+if len(a) < 11:
+    print("enter longer string")
+else:
+    print("too long")
+
+print("end charctrs are ", a[0], a[len(a)-1])
