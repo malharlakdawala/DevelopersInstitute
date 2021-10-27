d786f1c2936776d61274c1aa34eb03997b85ede8
malhar <malharlakdawala@gmail.com>
2021-10-27 19:27:41 +0530

    Day 3 exc2

---
+++ Week4/Day3/Exc2.py
@@ -0,0 +1,30 @@
+# Exercise 1,2,3: Birthday Look-Up
+# birthdays = {"Malhar":"20/11/89","Sonakshi":"30/11/90","Rohan":"23/03/93","Vaibhav":"27/08/89","Nirali":"01/05/91"}
+# #print(birthdays.keys())
+# new_name = input("Add new name to add in the list ")
+# new_birthdate = input("Add the birthdate to add in the list ")
+# birthdays[new_name]=new_birthdate
+# print(birthdays)
+# name=input(f'Enter name from {birthdays.keys()} ')
+# if name in birthdays.keys():
+#     print(f'birtdate of {name} is {birthdays[name]}')
+# else:
+#     print("name not in list")
+
+# Exc 4 Fruit shop
+# import random
+# items = {"banana": 4,
+#          "apple": 2,
+#          "orange": 1.5,
+#          "pear": 3
+#          }
+# sum=0;
+# for n in items:
+#     items[n]={}
+#     items[n]["price"]=random.randint(1,9)
+#     items[n]["stock"]=random.randint(1,9)
+#     sum+=items[n]["price"]*items[n]["stock"]
+# print(items)
+# print(sum)
+
+#Exercise 5 : Cars
