49a4c92025b50019cc49ef83d2e262c04e81a363
malhar <malharlakdawala@gmail.com>
2021-10-30 23:44:48 +0530

    js adv

--- Week4/Javascript/Exc1/scripts.js
+++ Week4/Javascript/Exc1/scripts.js
@@ -13,7 +13,7 @@
 // let experiencePoints = () => winBattle()? 10:1;
 // console.log(experiencePoints())
 
-//Exercise 3 : Colors
+// Exercise 3 : Colors
 // let colors = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
 // console.log(colors.includes("Violet"))
 //
@@ -26,21 +26,15 @@
 // function printData(item,index){
 //         console.log(`${index+1}# Choice of colour is ${item}`)
 // }
-//Exercise 4 : Colors #2
-// let color = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
-// let ordinal = ["th","st","nd","rd"];
-//
-// color.forEach(function (item,index){
-//     if (index==0){
-//         console.log(`${index+1}${ordinal[1]} choice is ${item}`)
-//     } else if (index==1){
-//         console.log(`${index+1}${ordinal[2]} choice is ${item}`)
-//     } else if (index==2){
-//         console.log(`${index+1}${ordinal[3]} choice is ${item}`)
-//     }else{
-//          console.log(`${index+1}${ordinal[0]} choice is ${item}`)
-//     }
-// })
+// Exercise 4 : Colors #2
+let color = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
+let ordinal = ["th","st","nd","rd"];
+
+color.forEach( (item,i,arr1)=>{
+    let str=(i<3)?ordinal[i+1]:ordinal[0]
+    arr1[i]= `${i+1}${str} choice is ${item}`
+})
+console.log(color)
 
 //Exercise 5 : Is It A String ?
 // function isString(str){
@@ -50,13 +44,13 @@
 // console.log(isString([1, 2, 4, 0]));
 
 //Exercise 6 : Bank Details
-let bankAmount = 1000;
-const vat = 0.17;
-let details = ["+200", "-100", "+146", "+167", "-2900"];
-details.forEach((item,index)=>{
-    let element = Number(item)
-    element<0?bankAmount=bankAmount+element+element*vat:bankAmount=bankAmount+element;
-    console.log(bankAmount)
-})
-console.log(bankAmount)
+// let bankAmount = 1000;
+// const vat = 0.17;
+// let details = ["+200", "-100", "+146", "+167", "-2900"];
+// details.forEach((item,index)=>{
+//     let element = Number(item)
+//     element<0?bankAmount=bankAmount+element+element*vat:bankAmount=bankAmount+element;
+//     console.log(bankAmount)
+// })
+// console.log(bankAmount)
 
