6641c3489b578ef92f5e70e5c4089ba8fc7480b0
malhar <malharlakdawala@gmail.com>
2021-11-01 21:05:20 +0530

    oops

--- Week5/Python/Day1/Dailychallenge.py
+++ Week5/Python/Day1/Dailychallenge.py
@@ -1,39 +1,36 @@
 class Farm:
-    def __init__(self,name):
-        self.name=name
-        self.store_list={}
+    def __init__(self, name):
+        self.name = name
+        self.store_list = {}
 
-    def add_animal(self,animal_name,qty=1):
+    def add_animal(self, animal_name, qty=1):
         if animal_name in self.store_list:
-            self.store_list[animal_name]+=qty
+            self.store_list[animal_name] += qty
         else:
-            self.store_list[animal_name]=qty
-
-
-    #checks keys
-
-
-    #    if key in [sub for ele in ]
-        # for i in self.store_list:
-          #     if animal_name == i[0]:
-        #         print("hi")
-        #     else: print("no hi")
-
-            # if self.store_list
-            #     print("hi")
-            # # if list.__contains__(animal_name):
-            # #     print("hi")
-            # #     self.store_list[i].qty = self.store_list[i].qty + qty
-            # else:
-            #     print("hello")
-            #     self.store_list.append({animal_name: qty})
-            # print(self.store_list)
-
+            self.store_list[animal_name] = qty
 
     def get_info(self):
-        return "E-I-E-I-0!"
+        str = ""
+        for k in self.store_list:
+            str = str + f"{k}:{self.store_list[k]} \n"
+
+        str = str + "E-I-E-I-0!"
+        return str
+    def get_animal_types(self):
+        return self.get_info()
+
+    def get_short_info(self):
+        str=""
+        for k in self.store_list:
+            str = str + ", " + k
+        return f"McDonald’s farm has {str}"
+
+
 
 macdonald = Farm("McDonald")
+macdonald.add_animal('cow', 5)
 macdonald.add_animal('sheep')
-macdonald.add_animal('sheep',5)
+macdonald.add_animal('sheep')
+macdonald.add_animal('goat', 12)
 print(macdonald.get_info())
+print(macdonald.get_short_info())
