a49055fe89db20db474b9ede5931fc8f65754bf5
malhar <malharlakdawala@gmail.com>
2021-11-07 18:24:59 +0530

    db

---
+++ Week6/Day1/Exc3.sql
@@ -0,0 +1,8 @@
+--insert into students(last_name,first_name,birth_date) values ('Benichou','Marc','02/11/1989');
+--insert into students(last_name,first_name,birth_date) values ('Cohen','Yoan','03/12/2010');
+--insert into students(last_name,first_name,birth_date) values ('Benichou','Lea','27/07/1987');
+--insert into students(last_name,first_name,birth_date) values ('Dux','Amelia','07/04/1996');
+--insert into students(last_name,first_name,birth_date) values ('Grez','David','14/06/2003');
+--insert into students(last_name,first_name,birth_date) values ('Simpson','Omer','03/10/1980');
+
+select * from students ORDER BY last_name ASC limit 4
