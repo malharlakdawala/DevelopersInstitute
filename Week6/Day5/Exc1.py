5def9b8aa03db5f461b55ce00db2e8e0e1aa1848
malhar <malharlakdawala@gmail.com>
2021-11-13 23:26:26 +0530

    db

--- Week6/Day5/Exc1.py
+++ Week6/Day5/Exc1.py
@@ -5,6 +5,8 @@ USERNAME = 'postgres'
 PASSWORD = 'root'
 DATABASE = 'MenuPython1'
 
+connection = psycopg2.connect(host=HOSTNAME, user=USERNAME, password=PASSWORD, dbname=DATABASE)
+cursor = connection.cursor()
 
 class MenuItem:
     def __init__(self):
@@ -12,37 +14,43 @@ class MenuItem:
 
     def save(self, name1, price1):
         query = f"INSERT INTO menuitem (name,price) values ('{name1}',{price1});"
-        return self.run_query(query)
+        cursor.execute(query)
+        connection.commit()
+        print("item added succesfully ")
 
     def delete(self, name):
         query = f"delete from menuitem where name='{name}';"
-        return self.run_query(query)
+        cursor.execute(query)
+        connection.commit()
+        print("item deleted succesfully ")
 
     def update(self, name, price):
         query = f"update menuitem set price={price} where name='{name}';"
-        return self.run_query(query)
+        cursor.execute(query)
+        connection.commit()
+        print("item updated succesfully ")
 
     def all(self):
+        print("full data is: ")
         query = f"select*from menuitem"
-        result= self.run_query(query)
+        cursor.execute(query)
+        connection.commit()
+        result= cursor.fetchall()
         for i in result:
             print(i)
 
     def get_by_name(self, name):
         query = f"select*from menuitem where name='{name}';"
-
-    def run_query(self, query):
-        connection = psycopg2.connect(host=HOSTNAME, user=USERNAME, password=PASSWORD, dbname=DATABASE)
-        cursor = connection.cursor()
         cursor.execute(query)
         connection.commit()
-        # if cursor.fetchall():
-        #     results = cursor.fetchall()
-        # else: results = ''
-        connection.close()
-        # return results
-
+        result = self.run_query(query)
+        for i in result:
+            print(i)
 
 item = MenuItem()
-item.save('Burger', 55)
-#item.all()
+# item.save('Veggie Burger', 36)
+# item.save('Dosa', 46)
+# item.save('Idli', 32)
+item.update('Idli', 58)
+item.all()
+connection.close()
