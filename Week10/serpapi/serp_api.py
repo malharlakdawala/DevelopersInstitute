a40885564da1ca4c851833488fab099ea7e12d99
malhar <malharlakdawala@gmail.com>
2021-12-22 17:43:11 +0530

    bootstrap testing

--- Week10/serpapi/serp_api.py
+++ Week10/serpapi/serp_api.py
@@ -9,16 +9,21 @@ secret_api = '309b109ea8fc68c4b3b2ce8b89d85bb9f9cf975f59359'
 #     "api_key": "309b109ea8fc68c4b3b2ce8b89d85bb9f9cf975f59359bf"
 # }
 
+# params = {
+#   "engine": "google_maps",
+#   "q": "kirana store",
+#   "ll": "@25.771315,73.323685,15.1z",
+#   "type": "search",
+#   "api_key": "309b109ea8fc68c4b3b2ce8b89d85bb9f9cf975f59359bf6c09"
+# }
+
+
 params = {
-  "engine": "google_maps",
-  "q": "kirana store",
-  "ll": "@25.771315,73.323685,15.1z",
-  "type": "search",
-  "api_key": "309b109ea8fc68c4b3b2ce8b89d85bb9f9cf975f59359bf6c09"
+  "q": "bank",
+  "location": "400101",
+  "api_key": "309b109ea8fc68c4b3b2ce8b89d85bb9f9cf975f59359bf6c09167d8c6a4ffc2"
 }
-
-
 search = GoogleSearch(params)
-results = search.get_dict()
+results = search.get_json()
 # local_results = results['local_results']
 print(results)
--- Week10/smsapi.py
+++ Week10/smsapi.py
@@ -1,6 +1,6 @@
 import requests
 
-URL = "http://2factor.in/API/V1/293832-67745-11e5-88de-5600000c6b13/ADDON_SERVICES/SEND/TSMS"
-data = {'From': "TFCTOR", 'To': "9769494336", 'Msg': "Hello World"}
+URL = "http://2factor.in/API/V1/3adf9525-efb4-11eb-8089-0200cd936042/ADDON_SERVICES/SEND/TSMS"
+data = {'From': "ERSSTD", 'To': "9769494336","TemplateName":"OTPVerification3" , "Var1"}
 r = requests.post(url = URL, data = data)
 print(r.text)
